d97a72ed5aaeb78158d52727bbe35da919684fb3
jwu <jwu@wujian.handpay.com.cn>
2017-12-27 13:09:52 +0800

    使用注解方式引用dubbo服务

--- SpringBootDemo/src/main/java/com/hpay/springboot/quickstart/HelloWorld.java
+++ SpringBootDemo/src/main/java/com/hpay/springboot/quickstart/HelloWorld.java
@@ -5,12 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.hpay.springboot.dubbo.EchoService;
+
 @SpringBootApplication
 @RestController
 public class HelloWorld {
+	
+	@Reference(version="1.0.0")
+	private EchoService echoservice;
 
 	@RequestMapping("/hello")
 	public String hello(){
+		echoservice.echo("hello");
 		return "HelloWorld";
 	}
 	
